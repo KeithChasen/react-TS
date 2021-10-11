44dabad3e6cbee6548cef00f11ffca5ae82edbff
keithchasen <keithchasen89@gmail.com>
2021-10-11 15:34:13 +0300

    changing state in todo form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const TodoForm: React.FC = () => (
-  <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Title"/>
-      <label htmlFor="title" className='active'>Title</label>
-  </div>
-);
+export const TodoForm: React.FC = () => {
+    const [title, setTitle] = useState<string>('');
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTitle(e.target.value);
+    };
+
+    return (
+        <div className="input-field mt2">
+            <input value={title} onChange={changeHandler} type="text" id="title" placeholder="Title"/>
+            <label htmlFor="title" className='active'>Title</label>
+        </div>
+    )
+};
