8cddcb3c1930017ba018e310fddd6666edf2fe97
keithchasen <keithchasen89@gmail.com>
2021-10-11 17:17:32 +0300

    useRef for handling input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 
 export const TodoForm: React.FC = () => {
-    const [title, setTitle] = useState<string>('');
-
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setTitle(e.target.value);
-    };
+    const ref = useRef<HTMLInputElement>(null);
 
     const keyPressHandler = (e: React.KeyboardEvent) => {
         if (e.key === 'Enter') {
-            console.log(title)
-            setTitle('')
+            console.log(ref.current!.value);
+            ref.current!.value = '';
         }
     };
 
     return (
         <div className="input-field mt2">
             <input
-                value={title}
-                onChange={changeHandler}
+                ref={ref}
                 onKeyPress={keyPressHandler}
                 type="text"
                 id="title"
