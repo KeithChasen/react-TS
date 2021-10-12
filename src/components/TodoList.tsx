dec7f84b8e5fec16381d40282269434cd49e231a
keithchasen <keithchasen89@gmail.com>
2021-10-12 17:17:07 +0300

    display todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,11 +9,15 @@ export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
     return (
         <ul>
             {todos.map(todo => {
+                const classes = ['todo'];
+                if (todo.completed) {
+                    classes.push('completed')
+                }
                 return (
-                    <li className="todo">
+                    <li className={classes.join(" ")} key={todo.id}>
                         <label htmlFor="">
-                            <input type="checkbox"/>
-                            <span></span>
+                            <input type="checkbox" checked={todo.completed}/>
+                            <span>{todo.title}</span>
                             <i className="material-icons red-text">delete</i>
                         </label>
                     </li>
