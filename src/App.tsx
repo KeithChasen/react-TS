bb1aba2a7071ff64902095d470ddb2c96abf97f5
keithchasen <keithchasen89@gmail.com>
2021-10-12 17:05:33 +0300

    adding todo to todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import { Navbar } from "./components/Navbar";
-import {TodoForm} from "./components/TodoForm";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces/interfaces";
 
 const App: React.FC = () => {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState<ITodo[]>([]);
 
     const addHandler = (title: string) => {
-        console.log("Here title: ", title)
-    }
+        const newTodo: ITodo = {
+            title: title,
+            id: Date.now(),
+            completed: false
+        };
+        setTodos([newTodo, ...todos])
+    };
 
     return (
         <>
             <Navbar/>
             <div className="container">
                 <TodoForm onAdd={addHandler}/>
+                <TodoList todos={todos}/>
             </div>
         </>
     );
