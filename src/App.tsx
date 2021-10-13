a8309a8efd013d30d0879c80dcacef75fe211bda
keithchasen <keithchasen89@gmail.com>
2021-10-13 10:56:32 +0300

    using router for navigation between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Navbar } from "./components/Navbar";
+import { Todos } from "./pages/Todos";
+import { About } from "./pages/About";
 
 const App: React.FC = () => {
     return (
-        <>
+        <BrowserRouter>
             <Navbar/>
             <div className="container">
+                <Switch>
+                    <Route component={Todos} path='/' exact/>
+                    <Route component={About} path='/about' />
+                </Switch>
             </div>
-        </>
+        </BrowserRouter>
     );
 };
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 
 export const About: React.FC = () => {
-    return (<><h1>Info page</h1></>)
+    const history = useHistory();
+    return (
+        <>
+            <h1>Info page</h1>
+            <button className="btn" onClick={() => history.push('/')}>Back</button>
+        </>
+    )
 };
